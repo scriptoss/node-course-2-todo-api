e00e1bcb672675ba2c05de16846fa80622095003
Kenneth <scriptoss@gmail.com>
2019-01-02 17:19:35 +1300

    Add DELETE /users/me/token route

--- server/models/users.js
+++ server/models/users.js
@@ -53,6 +53,17 @@ UserSchema.methods.generateAuthToken = function () {
   });
 }
 
+UserSchema.methods.removeToken = function(token) {
+  //$pull mongo DB function to pull tiems in an array
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  })
+}
+
 UserSchema.statics.findByToken = function(token) {
   var User = this;
   var decoded;
--- server/server.js
+++ server/server.js
@@ -126,6 +126,14 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 })
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 //POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
@@ -133,7 +141,7 @@ app.post('/users/login', (req, res) => {
   User.findByCredentials(body.email, body.password).then((user) =>{
     return user.generateAuthToken().then((token) => {
       res.header('x-auth', token).send(user);
-    });    
+    });
   }).catch((e) => {
     res.status(400).send();
   });
